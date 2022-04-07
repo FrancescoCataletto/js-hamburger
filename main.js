6a42a262a63a91a386448ba7052b1f8742342d42
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-07 17:08:48 +0200

    menu disappearing

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ const hambMenuBars = document.querySelector(".fa-bars");
 hambMenuBars.addEventListener('click', function(){
     hambMenu.classList.add("active");
 });
+
+const hambMenuX = document.querySelector(".fa-times");
+
+hambMenuX.addEventListener('click', function(){
+    hambMenu.classList.remove("active");
+})
