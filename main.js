0bb8dec80ebc875a13c608b28822860c813be2c3
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-07 17:07:16 +0200

    menu appearing

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const hambMenu = document.querySelector(".hamburger-menu");
 
 const hambMenuBars = document.querySelector(".fa-bars");
 
+hambMenuBars.addEventListener('click', function(){
+    hambMenu.classList.add("active");
+});
